ab7926b9065b23f24de6ae100843373ecaf9e9eb
btffh <btffhx@gmail.com>
2020-11-14 20:13:51 +0300

    Partially implement task 5 and task 6 task

--- task5.py
+++ task5.py
@@ -4,8 +4,10 @@
 # длинная последовательность элементов массива, простирающаяся слева направо и такая, что
 # каждый следующий элемент больше предыдущего.
 
+from typing import List, Tuple
 
-if __name__ == "__main__":
+
+def maxSeq(seq: List[int]) -> Tuple[int, List[int]]:
     # временная сложность алгоритма - O(n)
     # алгоритм заключается в переборе всей последовательности елементов и сравнении:
     # 1. на каждом этапе текущее число сравнивается с предыдущем
@@ -14,24 +16,62 @@ if __name__ == "__main__":
     #        новой максимальной последовательностью
     #   1.2. если текущее число меньше, то увеличивается счетчик длины текущей последовательности,
     #        предыдущий элемент заменяется текущем, происходит переход к следующему элементу
-
-    arr = [1, 2, 3, 1, 4, 5, 6, 7, 1, 2, 3, 4]
     maxCount, count = 0, 1
-    prev = arr[0]
-    analyticLogestArr = []
-    analyticArr = [arr[0]]
-    for el in arr[1:]:
+    prev = seq[0]
+    analyticLogestSeq = []
+    analyticSeq = [seq[0]]
+    for el in seq[1:]:
         if el > prev:
             count += 1
-            analyticArr.append(el)
+            analyticSeq.append(el)
         else:
             if count > maxCount:
                 maxCount = count
-                analyticLogestArr = analyticArr[:]
+                analyticLogestSeq = analyticSeq[:]
             count = 1
-            analyticArr = [el]
+            analyticSeq = [el]
         prev = el
 
-    print(f"{maxCount}: {str(analyticLogestArr)}")
+    return maxCount, analyticLogestSeq
+
+
+def maxSeqSpaces(seq: List[int]) -> Tuple[int, List[int]]:
+    minEl, minElInd = seq[0], 0
+    for ind, el in enumerate(seq):
+        if el < minEl:
+            minEl, minElInd = el, ind
+
+    countMax = 1
+    prev = seq[minEl]
+    analyticLongestSeq = [minEl]
+    for el in seq[minElInd + 1 :]:
+        if el > prev:
+            countMax += 1
+            analyticLongestSeq.append(el)
+            prev = el
+
+    return countMax, analyticLongestSeq
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 1, 4, 5, 6, 7, 1, 2, 3, 4]
+    countMax, analyticLongestSeq = maxSeq(arr)
+    print(f"{countMax}: {str(analyticLongestSeq)}")
+    # output >>>
+    # 5: [1, 4, 5, 6, 7]
+
+    countMax, analyticLongestSeq = maxSeqSpaces(arr)
+    print(f"{countMax}: {str(analyticLongestSeq)}")
+    # output >>>
+    # 6: [1, 3, 4, 5, 6, 7]
+
+    arr = [6, 2, 5, 1, 7, 4, 8, 3]
+    countMax, analyticLongestSeq = maxSeq(arr)
+    print(f"{countMax}: {str(analyticLongestSeq)}")
+    # output >>>
+    # 5: [1, 4, 5, 6, 7]
+
+    countMax, analyticLongestSeq = maxSeqSpaces(arr)
+    print(f"{countMax}: {str(analyticLongestSeq)}")
     # output >>>
     # 5: [1, 4, 5, 6, 7]
