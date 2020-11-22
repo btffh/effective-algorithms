2a498003a09102792136ff282d4341b77a5ee5b0
btffh <btffhx@gmail.com>
2020-11-22 13:40:30 +0300

    Implement task 7

--- task7.py
+++ task7.py
@@ -11,5 +11,108 @@
 # количеством запросов. Приведите алгоритм вычисления медианы с количеством запросов
 # не более O(log n).
 
+from typing import List, Tuple, Dict
+import itertools
+
+
+def getMedianFlatten(db1: List[int], db2: List[int]) -> Tuple[int, Dict[str, int]]:
+    """
+    Функция создает из двух отсортированных массивов один при помощи перебора
+    элементов и вставки значений:
+        1. i, j -индексы первого и второго массивов соответственно
+        2. пока i < длина массива 1 и j < длина массива 2:
+            2.1. если i-й элемент первого массива меньше j-го элемента второго
+                массива, то вставялем его в результирующий массив и увеличиваем
+                индекс i на 1
+            2.2. если j-й элемент второго массива меньше i-го элемента первого
+                массива, то вставялем его в результирующий массив и увеличиваем
+                индекс j на 1
+        3. возвращаем медиану
+    Сложность работы алгоритма - не более O(2n) - время перебораобоих массивов.
+
+    :param db1: массив
+    :type db1: List[int]
+    :param db2: массив
+    :type db2: List[int]
+    :raises ValueError: длина массивов не равна
+    :return: (медиана, статистика)
+    :rtype: Tuple[int, Dict[str, int]]
+    """
+    if len(db1) != len(db2):
+        raise ValueError
+
+    stats: Dict[str, int] = {"comparisons": 0, "insertions": 0}
+    i, j = 0, 0
+    entire: List[int] = []
+    while i < len(db1) and j < len(db1):
+        stats["comparisons"] += 1
+        stats["insertions"] += 1
+        if db1[i] < db2[j]:
+            entire.append(db1[i])
+            i += 1
+        else:
+            entire.append(db2[j])
+            j += 1
+
+    if i < len(db1):
+        stats["comparisons"] += 1
+        stats["insertions"] += len(db1) - i
+        entire.extend(db1[i:])
+    if j < len(db2):
+        stats["comparisons"] += 1
+        stats["insertions"] += len(db2) - j
+        entire.extend(db2[j:])
+
+    if len(entire) % 2 == 1:
+        stats["comparisons"] += 1
+        return entire[len(entire) // 2], stats
+    return 0.5 * (entire[len(entire) // 2 - 1] + entire[len(entire) // 2]), stats
+
+
+def incSingleStat(a: int, stats: Dict[str, int]) -> bool:
+    stats["insertions"] += 1
+    stats["comparisons"] += 1
+    return a
+
+
+def getMedianFlattenSort(db1: List[int], db2: List[int]) -> Tuple[int, Dict[str, int]]:
+    """
+    Функция создает из двух массивов один, а затем сортирует его и возвращает
+    медианное значение. Сложность O(2n + n*log(n))
+
+    :param db1: массив
+    :type db1: List[int]
+    :param db2: массив
+    :type db2: List[int]
+    :raises ValueError: длина массивов не равна
+    :return: (медиана, статистика)
+    :rtype: Tuple[int, Dict[str, int]]
+    """
+    if len(db1) != len(db2):
+        raise ValueError
+
+    stats: Dict[str, int] = {"comparisons": 0, "insertions": 0}
+    stats["insertions"] = len(db1) * 2
+    entire: List[int] = sorted(
+        itertools.chain(db1, db2), key=lambda a: incSingleStat(a, stats)
+    )
+    stats["comparisons"] += 1
+    if len(entire) % 2 == 1:
+        return entire[len(entire) // 2], stats
+    return 0.5 * (entire[len(entire) // 2 - 1] + entire[len(entire) // 2]), stats
+
+
 if __name__ == "__main__":
-    pass
+    # Исходя из условия задачи мы получаем k-е по возрастанию число, то есть мы можем
+    # оперировать двумя возрастающими массивами и каждый раз брать элемент с индексом k,
+    # что и будет соответствовать k-му значению в порядке возрастания.
+
+    print("--- Length 17 * 2 ---")
+    # length == 17
+    db1 = [0, 1, 6, 7, 9, 10, 12, 12, 17, 26, 28, 37, 42, 44, 46, 49, 50]
+    # length == 17
+    db2 = [1, 4, 7, 14, 14, 15, 16, 18, 20, 20, 24, 24, 29, 33, 39, 39, 46]
+    res = getMedianFlattenSort(db1, db2)
+    print(f"flattenSort: {res}")
+    res = getMedianFlatten(db1, db2)
+    print(f"flatten: {res}")
