e8b30381c0552f98e9d141fcf09d8eff0d9595da
btffh <btffhx@gmail.com>
2020-11-22 15:15:56 +0300

    Add more comments

--- task7.py
+++ task7.py
@@ -1,4 +1,4 @@
-# Яким Алексей ПИ17-1
+# Яким Алексей ПИ17-1 Вариант 1
 #
 # Требуется проанализировать информацию из двух разных баз данных. Каждая база данных
 # содержит n числовых значений (так что общее количество значений равно 2n); будем
@@ -28,7 +28,7 @@ def getMedianFlatten(db1: List[int], db2: List[int]) -> Tuple[int, Dict[str, int
                 массива, то вставялем его в результирующий массив и увеличиваем
                 индекс j на 1
         3. возвращаем медиану
-    Сложность работы алгоритма - не более O(2n) - время перебораобоих массивов.
+    Сложность работы алгоритма - O(2n) - время перебора обоих массивов.
 
     :param db1: массив
     :type db1: List[int]
@@ -69,7 +69,7 @@ def getMedianFlatten(db1: List[int], db2: List[int]) -> Tuple[int, Dict[str, int
     return 0.5 * (entire[len(entire) // 2 - 1] + entire[len(entire) // 2]), stats
 
 
-def incSingleStat(a: int, stats: Dict[str, int]) -> bool:
+def incStatsSingleOperation(a: int, stats: Dict[str, int]) -> bool:
     stats["insertions"] += 1
     stats["comparisons"] += 1
     return a
@@ -94,7 +94,7 @@ def getMedianFlattenSort(db1: List[int], db2: List[int]) -> Tuple[int, Dict[str,
     stats: Dict[str, int] = {"comparisons": 0, "insertions": 0}
     stats["insertions"] = len(db1) * 2
     entire: List[int] = sorted(
-        itertools.chain(db1, db2), key=lambda a: incSingleStat(a, stats)
+        itertools.chain(db1, db2), key=lambda a: incStatsSingleOperation(a, stats)
     )
     stats["comparisons"] += 1
     if len(entire) % 2 == 1:
@@ -107,12 +107,39 @@ if __name__ == "__main__":
     # оперировать двумя возрастающими массивами и каждый раз брать элемент с индексом k,
     # что и будет соответствовать k-му значению в порядке возрастания.
 
+    # Общий алгоритм описан в документации к функции getMedianFlatten, ниже копия описания:
+    # 1. i, j -индексы первого и второго массивов соответственно
+    # 2. пока i < длина массива 1 и j < длина массива 2:
+    #     2.1. если i-й элемент первого массива меньше j-го элемента второго
+    #         массива, то вставялем его в результирующий массив и увеличиваем
+    #         индекс i на 1
+    #     2.2. если j-й элемент второго массива меньше i-го элемента первого
+    #         массива, то вставялем его в результирующий массив и увеличиваем
+    #         индекс j на 1
+    # 3. возвращаем медиану
+    #
+    # Сложность работы алгоритма - O(2n) - время перебора обоих массивов. При этом O(2n) будет
+    # эффективнее алгоритма с временной сложностью O(n*log(n)) для всех случаев, когда суммарная
+    # длина обоих массивов не менее 8 элементов.
+    # Так же стоит отметить, что рассматриваются отсортированные массивы, однако, исходя из
+    # условия, массивы могут быть не отсортированными, но будет реализована дополнительная функция,
+    # которая возвращает k-й возрастающий элемент массива, при этом время работы этой функции
+    # не учитывается.
+
     print("--- Length 17 * 2 ---")
     # length == 17
     db1 = [0, 1, 6, 7, 9, 10, 12, 12, 17, 26, 28, 37, 42, 44, 46, 49, 50]
     # length == 17
     db2 = [1, 4, 7, 14, 14, 15, 16, 18, 20, 20, 24, 24, 29, 33, 39, 39, 46]
+
+    # надежный и медленный вариант для проверки работы эффективного варианта
     res = getMedianFlattenSort(db1, db2)
-    print(f"flattenSort: {res}")
+    print(f"[flattenSort] median: {res[0]}, stats: {res[1]}")
+    # output >>>
+    # [flattenSort] median: 19.0, stats: {'comparisons': 35, 'insertions': 68}
+
+    # эффективный вариант поиска медианы
     res = getMedianFlatten(db1, db2)
-    print(f"flatten: {res}")
+    print(f"[flatten] median: {res[0]}, stats: {res[1]}")
+    # output >>>
+    # [flatten] median: 19.0, stats: {'comparisons': 32, 'insertions': 34}
